45dd26c6528d214e6955a5d16d46bcae453001b0
Desheye <aarealaafiaboyz@gmail.com>
2024-08-08 08:00:10 -0700

    semistandard working

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
+
 // Get the Movie ID from command line arguments
 const movieId = process.argv[2];
+const swapiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-const swapiUrl = `https:
 request(swapiUrl, (error, response, body) => {
   if (error) {
     console.error('Error fetching movie data:', error);
     process.exit(1);
   }
-
   if (response.statusCode !== 200) {
     console.error('Unexpected status code:', response.statusCode);
     process.exit(1);
   }
   const movieData = JSON.parse(body);
   const characters = movieData.characters;
+
   // Print character names
   characters.forEach((characterUrl) => {
     request(characterUrl, (charError, charResponse, charBody) => {
