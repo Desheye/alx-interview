4fd5cfe54b6c77c34ba99c26688ba433f2af7fdc
Desheye <aarealaafiaboyz@gmail.com>
2024-08-08 14:31:49 +0000

    star wars api lint

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 // Get the Movie ID from command line arguments
 const movieId = process.argv[2];
 
-// Construct the SWAPI URL for the movie
-const swapiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-// Make the HTTP request
+const swapiUrl = `https:
 request(swapiUrl, (error, response, body) => {
   if (error) {
     console.error('Error fetching movie data:', error);
@@ -19,10 +14,8 @@ request(swapiUrl, (error, response, body) => {
     console.error('Unexpected status code:', response.statusCode);
     process.exit(1);
   }
-
   const movieData = JSON.parse(body);
   const characters = movieData.characters;
-
   // Print character names
   characters.forEach((characterUrl) => {
     request(characterUrl, (charError, charResponse, charBody) => {
@@ -33,4 +26,3 @@ request(swapiUrl, (error, response, body) => {
     });
   });
 });
-
